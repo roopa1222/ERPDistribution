1961b72cc99a704624f3a893b55bbe6ab43fb2ca
karthik-1725 <karthikganesan1725@gmail.com>
2024-12-01 21:06:40 +0530

    KG-added filter

--- src/routes/v1/dsrInvoice-routes.ts
+++ src/routes/v1/dsrInvoice-routes.ts
@@ -6,7 +6,7 @@ import { IRoles } from "../../types/user";
 const router = Router();
 
 router.post('/add-dsr-invoice',
-    // authenticateToken([IRoles.SUPER_ADMIN, IRoles.ADMIN, IRoles.SALESMAN]),
+    authenticateToken([IRoles.SUPER_ADMIN, IRoles.ADMIN, IRoles.SALESMAN]),
     DsrInvoiceController.createDsrInvoice);
 
 router.put('/edit-dsr-invoice', authenticateToken([IRoles.SUPER_ADMIN, IRoles.ADMIN]), DsrInvoiceController.updateDsrInvoice);
@@ -16,7 +16,7 @@ router.get('/dsr-invoice', authenticateToken([IRoles.SUPER_ADMIN, IRoles.ADMIN, 
 router.get('/dashboard-count', authenticateToken([IRoles.SUPER_ADMIN, IRoles.ADMIN, IRoles.SALESMAN]), DsrInvoiceController.getDashBoardCountData)
 
 router.get('/dsr-invoice-excel-data', 
-    // authenticateToken([IRoles.SUPER_ADMIN, IRoles.ADMIN, IRoles.SALESMAN]), 
+    authenticateToken([IRoles.SUPER_ADMIN, IRoles.ADMIN, IRoles.SALESMAN]), 
     DsrInvoiceController.getDSRInvoiceExcelData);
 
 export default router;
